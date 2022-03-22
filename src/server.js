4ee7dd48ac245676462323b0bcfe9957e7c0ac05
trinhquangha <trinhha732000@gmail.com>
2022-03-22 12:21:35 +0700

    Handle CORS

--- src/server.js
+++ src/server.js
@@ -1,26 +1,31 @@
-import express from 'express'
-import { connectDB } from '*/config/mongodb'
-import { env } from '*/config/environment'
-import { apiV1 } from '*/routes/v1'
+import express from 'express';
+import cors from 'cors';
+
+import { corsOptions } from '*/config/cors';
+import { connectDB } from '*/config/mongodb';
+import { env } from '*/config/environment';
+import { apiV1 } from '*/routes/v1';
 
 connectDB()
 	.then(() => console.log('Connected successfully to database first!'))
 	.then(() => bootServer())
 	.catch((error) => {
-		console.log(error)
-		process.exit(1)
-	})
+		console.log(error);
+		process.exit(1);
+	});
 
 const bootServer = () => {
-	const app = express()
+	const app = express();
+
+	app.use(cors(corsOptions));
 
 	// Enable req.body data
-	app.use(express.json())
+	app.use(express.json());
 
 	//Use APIs v1
-	app.use('/v1', apiV1)
+	app.use('/v1', apiV1);
 
 	app.listen(env.APP_PORT, env.APP_HOST, () => {
-		console.log(`Server is listening on ${env.APP_HOST}:${env.APP_PORT}/`)
-	})
-}
+		console.log(`Server is listening on ${env.APP_HOST}:${env.APP_PORT}/`);
+	});
+};
